2af0d34a56390539716b8a1b701f26575c51d9a6
Maximiliano Barrionuevo <maxisbarrionuevo@gmail.com>
2024-02-23 23:10:03 -0300

    Start

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'ARS', nombre: 'Peso Argentino'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Estrerlina'},
+]
+
+export {
+    monedas
+}
